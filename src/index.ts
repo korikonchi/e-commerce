9284572215e2e911d2526beec03c49b9419e6ef6
Daniel Concha <korikonchi91@gmail.com>
2025-01-15 23:51:22 -0600

    feature(HTTP-SERVER) added https suport certificates index.ts configuration

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,11 @@ import express, { Application } from 'express'
 import 'express-async-errors'
 import Server from './server'
 import { email } from './globals/helpers/email'
-
 import path from 'path'
 import dotenv from 'dotenv'
+import https from 'https'
+import fs from 'fs'
+import { startConsumer } from './kafka'
 
 const env = process.env.NODE_ENV
 
@@ -20,10 +22,24 @@ class ShopApplication {
     const app: Application = express()
     const server: Server = new Server(app)
 
+    const options = {
+      key: fs.readFileSync(path.resolve(__dirname, '../certificates/key.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, '../certificates/cert.pem'))
+    }
+
+    const httpsServer = https.createServer(options, app)
+
+    const port = parseInt(process.env.PORT!) || 5050
+
+    process.env.NODE_ENV !== 'development' && startConsumer(process.env.KAFKA_TOPIC || 'my-topic')
+
+    httpsServer.listen(port, () => {
+      console.log(`server listener on port ${port}`)
+    })
+
     server.start()
   }
 }
 
 const shopApplication: ShopApplication = new ShopApplication()
-
 shopApplication.run()
